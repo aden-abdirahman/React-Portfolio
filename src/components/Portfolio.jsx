81400976e4a46e0fdeb6f872a39015b7c99bc13a
aden-abdirahman <aden.abdirahman45@gmail.com>
2022-04-27 13:03:35 -0400

    updating links

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -25,10 +25,10 @@ const Portfolio = () => {
                         Full Stack App
                         </span>
                         <div className='pt-8 text-center'>
-                            <a href="/">
+                            <a href="https://jamy-game-store.herokuapp.com/">
                                 <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
                             </a>
-                            <a href="/">
+                            <a href="https://github.com/Mohamed7Aden/JAMY-Games">
                                 <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
                             </a>
                         </div>
